086217165b5bc07da784c4909975ab19a9af0f47
Yesael <yesael_maldonado@comunidad.unam.mx>
2021-12-02 13:56:50 -0600

    clear

--- js/app.js
+++ js/app.js
@@ -4,17 +4,6 @@
    
     
     app.controller('listadoCtrl', ['$scope', '$http',function($scope,$http) {
-      $scope.contact={};
-      function makeid(length) {
-        var result           = '';
-        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        var charactersLength = characters.length;
-        for ( var i = 0; i < length; i++ ) {
-          result += characters.charAt(Math.floor(Math.random() * 
-     charactersLength));
-       }
-       return result;
-    }
       
 
 
